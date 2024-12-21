4dce383b4213e4aaaf3125054593cc11b49e583a
Dhanunjaya <163657146+codebydj@users.noreply.github.com>
2024-12-21 22:24:01 +0530

    Update scrollAnimation.js
    scroll error

--- scrollAnimation.js
+++ scrollAnimation.js
@@ -4,7 +4,7 @@ var sections = document.querySelectorAll(".section");
 // Function to check if a section is in view
 function isSectionInView(sec) {
   let top = window.scrollY;
-  let offset = sec.offsetTop-400;
+  let offset = sec.offsetTop - 400; // Adjust the offset as needed
   let height = sec.offsetHeight;
   return top >= offset && top < offset + height;
 }
@@ -14,8 +14,6 @@ function handleScroll() {
   sections.forEach((sec) => {
     if (isSectionInView(sec)) {
       sec.classList.add("showAnimate");
-      // Scroll the section into full view
-      sec.scrollIntoView({ behavior: 'smooth', block: 'start' });
     } else {
       sec.classList.remove("showAnimate");
     }
